a25407a76ab857c3210be6a2356cf3a803f80302
xc8pt <brolike636@gmail.com>
2026-01-29 22:48:07 +0300

    Add extension properties and functions

--- src/Main.kt
+++ src/Main.kt
@@ -1,23 +1,26 @@
 fun main(){
-    val question1 = Question<String>(
-        "Речка спятила с ума - По домам пошла сама. ___",
-        "водопровод",
-        Difficulty.MEDIUM
-    )
-    val question2 = Question<Boolean>(
-        "Небо зелёное. Правда или ложь",
-        false,
-        Difficulty.EASY
-    )
-    val question3 = Question<Int>(
-        "Сколько дней между полнолуниями?",
-        28,
-        Difficulty.HARD
-    )
-    println(question1.answer)
-    println(question2.answer)
-    println(question3.answer)
-    println(question1.toString())
-
-    println("${Quiz.answered} of ${Quiz.total} answered.")
+//    val question1 = Question<String>(
+//        "Речка спятила с ума - По домам пошла сама. ___",
+//        "водопровод",
+//        Difficulty.MEDIUM
+//    )
+//    val question2 = Question<Boolean>(
+//        "Небо зелёное. Правда или ложь",
+//        false,
+//        Difficulty.EASY
+//    )
+//    val question3 = Question<Int>(
+//        "Сколько дней между полнолуниями?",
+//        28,
+//        Difficulty.HARD
+//    )
+//    println(question1.answer)
+//    println(question2.answer)
+//    println(question3.answer)
+//    println(question1.toString())
+//
+//    println("${Quiz.answered} of ${Quiz.total} answered.")
+//    println(Quiz.progressText)
+    ///
+    Quiz.printProgressBar()
 }
--- src/Question.kt
+++ src/Question.kt
@@ -51,3 +51,12 @@ class Quiz{
         var answered: Int = 3
     }
 }
+val Quiz.StudentProgress.progressText: String
+    get() = "${answered} of ${total} answered"
+
+fun Quiz.StudentProgress.printProgressBar(){
+    repeat(Quiz.answered) {print("▓")}
+    repeat(Quiz.total - Quiz.answered) {print("▓")}
+    println()
+    println(Quiz.progressText)
+}
