f0d0bfd7704b560fb4b8a258a64f8ee840e73483
xc8pt <brolike636@gmail.com>
2026-01-29 23:44:09 +0300

    fold() function

--- src/Main.kt
+++ src/Main.kt
@@ -100,15 +100,20 @@ fun main(){
 //        println("${it.name} - $${it.price}")
 //    }
     ///
-    val groupedMenu = cookies.groupBy { it.softBaked }
-    val softBakedMenu = groupedMenu[true] ?: emptyList()
-    val crunchyMenu = groupedMenu[false] ?: emptyList()
-    println("Soft cookies:")
-    softBakedMenu.forEach {
-        println("${it.name} - $${it.price}")
+//    val groupedMenu = cookies.groupBy { it.softBaked }
+//    val softBakedMenu = groupedMenu[true] ?: emptyList()
+//    val crunchyMenu = groupedMenu[false] ?: emptyList()
+//    println("Soft cookies:")
+//    softBakedMenu.forEach {
+//        println("${it.name} - $${it.price}")
+//    }
+//    println("Crunchy cookies:")
+//    crunchyMenu.forEach {
+//        println("${it.name} - $${it.price}")
+//    }
+    ///
+    val totalPrice = cookies.fold(0.0) { total, cookie ->
+        total + cookie.price
     }
-    println("Crunchy cookies:")
-    crunchyMenu.forEach {
-        println("${it.name} - $${it.price}")
-    }
+    println("Total price: $${totalPrice}")
 }
